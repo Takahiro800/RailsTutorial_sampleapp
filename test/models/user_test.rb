9588e64d3bd19f5e0e56538ca7cae8bf1aaffbc2
Takahiro Abe <hirotaka081630@gmail.com>
2020-06-21 12:11:31 +0900

    emailカラムの一意性の定義とそのテスト

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -29,4 +29,29 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "a" * 244 + "@example.com"
     assert_not @user.valid?
   end
+
+  test "email validation should accept valid addresses" do
+    valid_adresses = %w[user@example.com USER@foo.Com A_US-ER@foo.bar.org
+                first.last@foo.jp alice+bob@baz.cn]
+    valid_adresses.each do |valid_adress|
+      @user.email = valid_adress
+      assert @user.valid?, "#{valid_adress.inspect} should be valid"
+    end
+  end
+
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,con user_atfoo.org user.name@example.
+                    foo@bar_baz.com foo@bar+bav.con]
+    invalid_addresses.each  do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
 end
