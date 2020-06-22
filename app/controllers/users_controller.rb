7cb85227e23ae94ea5b673e387d48b18d21f04aa
Takahiro Abe <hirotaka081630@gmail.com>
2020-06-23 06:17:27 +0900

    ユーザー登録に関する実装とテスト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user= User.new(params[:user])
+    @user= User.new(user_params)
     if @user.save
       #成功の処理をここに
     else
