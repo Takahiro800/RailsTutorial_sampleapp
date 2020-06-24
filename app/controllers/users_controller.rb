98d12073a73bd477c0956a68a22ee71edebfc7a7
Takahiro Abe <hirotaka081630@gmail.com>
2020-06-24 20:20:01 +0900

    ユーザー登録のviewの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
     @user= User.new(user_params)
     if @user.save
-      #成功の処理をここに
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
