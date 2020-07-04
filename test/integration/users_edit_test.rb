7daa2d1b1f7894e4039d23aa29ef2b09ca267e43
Takahiro Abe <hirotaka081630@gmail.com>
2020-07-05 05:27:29 +0900

    Finish user edit, update, index, and destroy actions

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -30,18 +30,6 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_equal email, @user.email
   end
 
-  test "should redirect update when not logged in" do
-    get edit_user_path(@user)
-    assert_not flash.empty?
-    assert_redirected_to login_url
-  end
-
-  test "shoud redirect update when not logged in" do
-    patch user_path(@user), params: { user: { name: @user.name, email: @user.email } }
-    assert_not flash.empty?
-    assert_redirected_to login_url
-  end
-
   test "successful edit with firendly forwarding" do
     get edit_user_path(@user)
     log_in_as(@user)
